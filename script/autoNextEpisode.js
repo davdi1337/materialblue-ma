dfdd9e4cb7677a7c637270b442c4deddfb640b07
davdi1337 <66253422+davdi1337@users.noreply.github.com>
2022-08-03 18:44:12 +0200

    újraírva az egész script

--- script/autoNextEpisode.js
+++ script/autoNextEpisode.js
@@ -1,72 +1,100 @@
-var x = document.querySelector("#MagyarAnime > div > div.plyr__controls > div.plyr__controls__item.plyr__time--duration.plyr__time").innerText;
-var y = document.querySelector("#MagyarAnime > div > div.plyr__controls > div.plyr__controls__item.plyr__time--current.plyr__time").innerText;
-var currentTime_div = document.querySelector("#MagyarAnime > div > div.plyr__controls > div.plyr__controls__item.plyr__time--current.plyr__time");
-var video_player = document.querySelector("#MagyarAnime > div > div.plyr__video-wrapper");
-var button = document.querySelector("#MagyarAnime > div > button");
-var nextEp = document.querySelector("body > div:nth-child(2) > section > div > div > div.col-lg-12 > div.anime__video__player > center > div:nth-child(1) > div.gomb_kovetkezo > a");
-var epTime = parseInt(x.slice(0, 2) * 60) + parseInt(x.slice(-2));
-var targetTime = epTime * 0.935; // 93,5% a rész teljes hosszából
-var countdown = 25; // visszaszámláló ideje
-
-const element = document.createElement("a");
-element.innerHTML = `Következő rész (${countdown})`;
-if (nextEp != null) {
-  element.href = nextEp.href; // ha nincs következő rész ne csináljon gombot
-}
-element.classList.add("kovetkezo_ep", "gomb_kovetkezo");
-element.style.color = "white";
-element.style.zIndex = 10;
-element.style.right = "5%";
-element.style.bottom = "10%";
-element.style.position = "absolute";
-element.style.padding = "10px 25px";
-element.style.minWidth = "250px";
-element.style.display = "block";
-element.style.transition = "opacity 1s";
-element.style.opacity = 0;
-element.style.boxShadow = "0 0 10px 0 rgb(0 0 0 / 20%), 0 12px 24px -4px rgb(0 0 0 / 12%)";
-
-var timer = setInterval(function () {
-  if (countdown <= 0) {
-    clearInterval(timer);
-    window.location = nextEp.href;
-  } else {
-    element.innerHTML = `Következő rész (${countdown})`;
-  }
-  if (document.querySelector("#MagyarAnime > div").classList.contains("plyr--paused")) {
-    countdown; // ha megállítják a lejátszást megáll a számláló
-  } else {
-    countdown -= 1; // ha elindul a lejátszás elindul a számláló
-  }
-}, 1000);
-
-function createButton() {
-  video_player.appendChild(element);
-  // timeout animation
+if (window.location.pathname.includes("epizod")) {
+  // 500ms timeout hogy biztosan lekérje a jó időt
   setTimeout(() => {
-    element.style.opacity = 1;
+    AutoNextEp();
   }, 500);
 }
 
-currentTime_div.addEventListener("DOMSubtreeModified", function () {
-  var currentTime_mins = parseInt(currentTime_div.innerText.slice(0, 2) * 60);
-  var currentTime_sec = parseInt(currentTime_div.innerText.slice(-2));
-  var currentTime = currentTime_mins + currentTime_sec;
-  var kovetkezo_ep_buttons = document.querySelectorAll(".kovetkezo_ep");
+function AutoNextEp() {
+  const player = document.querySelector(
+    "#MagyarAnime > div > div.plyr__video-wrapper"
+  );
+  const currentTime_el = document.querySelector(
+    "#MagyarAnime > div > div.plyr__controls > div.plyr__controls__item.plyr__time--current.plyr__time"
+  );
+  const epDuration_el = document.querySelector(
+    "#MagyarAnime > div > div.plyr__controls > div.plyr__controls__item.plyr__time--duration.plyr__time"
+  );
+  const nextEpisode = document.querySelector(
+    "body > div:nth-child(2) > section > div > div > div.col-lg-12 > div.anime__video__player > center > div:nth-child(1) > div.gomb_kovetkezo > a"
+  );
 
-  if (currentTime >= targetTime) {
-    // ne csináljon több gombot 1-nél
-    if (kovetkezo_ep_buttons[0] === null && kovetkezo_ep_buttons.length >= 1) {
-      return;
+  var epDuration_converted;
+  var currentTime_converted;
+  if (player === null) {
+    return; // ha nincs lejátszó ne csináljon semmit
+  } else {
+    currentTime_converted =
+      parseInt(currentTime_el.innerHTML.slice(0, 2) * 60) +
+      parseInt(currentTime_el.innerHTML.slice(-2));
+    epDuration_converted =
+      parseInt(epDuration_el.innerHTML.slice(0, 2) * 60) +
+      parseInt(epDuration_el.innerHTML.slice(-2));
+  }
+
+  const epTarget = epDuration_converted * 0.935; // honnan kezdődjön
+  var countdown = 25; // számláló
+
+  const nextEp_button = document.createElement("a");
+  nextEp_button.classList.add("gomb_kovetkezo", "nextep_button");
+  nextEp_button.innerHTML = `Következő rész (${countdown})`;
+  nextEp_button.style.position = "absolute";
+  nextEp_button.style.right = "5%";
+  nextEp_button.style.bottom = "10%";
+  nextEp_button.style.zIndex = 10;
+  nextEp_button.href = nextEpisode?.href;
+  nextEp_button.style.padding = "10px 25px";
+  nextEp_button.style.color = "white";
+  nextEp_button.style.minWidth = "250px";
+  nextEp_button.style.boxShadow =
+    "0 0 10px 0 rgb(0 0 0 / 20%), 0 12px 24px -4px rgb(0 0 0 / 12%)";
+
+  async function createButton() {
+    if (player === null) {
+      return; // ne csináljon gombot
     } else {
-      createButton(); // megcsinálja a gombot
-    }
-  } else if (currentTime <= targetTime) {
-    countdown = 25; // visszaszámláló alaphelyzet
-    if (element.parentNode !== null) {
-      element.parentNode.removeChild(element);
-    } else {
-      return;
+      player.appendChild(nextEp_button);
     }
   }
-});
+  async function removeButton() {
+    if (player === null) {
+      return; // ne csináljon semmit
+    } else if (document.querySelector(".nextep_button") !== null) {
+      nextEp_button.parentNode.removeChild(nextEp_button);
+      countdown = 25; // számláló reset
+    }
+  }
+  currentTime_el.addEventListener("DOMSubtreeModified", function () {
+    currentTime_converted =
+      parseInt(currentTime_el.innerHTML.slice(0, 2) * 60) +
+      parseInt(currentTime_el.innerHTML.slice(-2)); // jelenlegi idő frissítése automatikusan
+    if (currentTime_converted > epTarget) {
+      createButton();
+    } else if (currentTime_converted < epTarget) {
+      removeButton();
+    }
+  });
+  if (nextEpisode !== null) {
+    var timer = setInterval(function () {
+      if (currentTime_converted > epTarget) {
+        if (countdown <= 0) {
+          clearInterval(timer);
+          window.location = nextEpisode.href;
+        } else {
+          nextEp_button.innerHTML = `Következő rész (${countdown})`;
+        }
+        if (
+          document
+            .querySelector("#MagyarAnime > div")
+            .classList.contains("plyr--paused")
+        ) {
+          countdown; // ha megáll a lejátszás megáll a számláló
+        } else {
+          countdown -= 1; // ha elindul a lejátszás elindul a számláló
+        }
+      } else {
+        return;
+      }
+    }, 1000);
+  }
+}
